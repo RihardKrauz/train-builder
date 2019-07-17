721701f7522d70501176bf19c1f1da587c57e6ee
Chkalov Evgeny <e.chkalov@itransition.com>
2019-07-17 15:18:17 +0300

    init3

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
 const express = require('express');
+const { Client } = require('pg');
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello');
 });
 
+app.get('/test', (request, response) => {
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+    });
+
+    client.connect();
+
+    client.query('select * from train.exercise;', (err, res) => {
+        if (err) {
+            throw err;
+        }
+        let msg = '';
+        for (let row of res.rows) {
+            msg += JSON.stringify(row);
+        }
+
+        client.end();
+
+        response.send(msg);
+    });
+});
+
 app.listen(process.env.PORT);
 
 // const { Client } = require('pg');
-
-// const client = new Client({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: true
-// });
-
-// client.connect();
-
-// client.query('select * from train.exercise;', (err, res) => {
-//     if (err) {
-//         throw err;
-//     }
-
-//     for (let row of res.rows) {
-//         console.log(JSON.stringify(row));
-//     }
-
-//     client.end();
-// });
