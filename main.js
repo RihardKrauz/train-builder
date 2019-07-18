1852a80b85203881346d74293b09415cb07c1804
Chkalov Evgeny <e.chkalov@itransition.com>
2019-07-18 16:09:15 +0300

    init3

--- main.js
+++ main.js
@@ -7,26 +7,48 @@ app.get('/', (req, res) => {
 });
 
 app.get('/test', (request, response) => {
-    const client = new Client({
-        connectionString: process.env.DATABASE_URL,
-        ssl: true
-    });
+    try {
+        const client = new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: true
+        });
 
-    client.connect();
+        client.connect();
 
-    client.query('select * from train.exercise;', (err, res) => {
-        if (err) {
-            throw err;
-        }
-        let msg = '';
-        for (let row of res.rows) {
-            msg += JSON.stringify(row);
-        }
+        client.query('select * from train.exercise;', (err, res) => {
+            if (err) {
+                throw err;
+            }
+            let msg = '';
+            for (let row of res.rows) {
+                msg += JSON.stringify(row);
+            }
 
-        client.end();
+            client.end();
+            const html = `
+                    <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="utf-8" />
+                        <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                        />
+                        <meta name="theme-color" content="#000000" />
 
-        response.send(msg);
-    });
+                        <title>New app</title>
+                    </head>
+                    <body>
+                        <noscript>You need to enable JavaScript to run this app.</noscript>
+                        <div id="root">${msg}</div>
+                    </body>
+                    </html>
+            `;
+            response.send(html);
+        });
+    } catch (ex) {
+        response.send(ex);
+    }
 });
 
 app.listen(process.env.PORT);
