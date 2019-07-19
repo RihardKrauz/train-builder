226e38cd3fc06f162f8a34e3a0771dd8723a4225
Chkalov Evgeny <e.chkalov@itransition.com>
2019-07-19 14:09:14 +0300

    autocommit

---
+++ actions/debug.js
@@ -0,0 +1,31 @@
+const debugLayout = require('../layouts/debug.html');
+const PgClient = require('./services/pg-client');
+
+function doDebug(request, response) {
+    const query = request.query.queryData;
+
+    if (!query) {
+        const html = debugLayout.split('{msg}').join('Query is empty');
+        response.send(html);
+    } else {
+        const pgClient = new PgClient();
+        pgClient
+            .runQueryAsync(query)
+            .then(res => {
+                let msg = '';
+                if (res.rows) {
+                    for (let row of res.rows) {
+                        msg += JSON.stringify(row);
+                    }
+                }
+
+                const html = debugLayout.split('{msg}').join(msg);
+                response.send(html);
+            })
+            .catch(errP => {
+                response.send(errP);
+            });
+    }
+}
+
+module.exports = doDebug;
--- main.js
+++ main.js
@@ -1,47 +1,19 @@
-// js imports
 const express = require('express');
-const PgClient = require('./services/pg-client');
 const fs = require('fs');
 
 require.extensions['.html'] = (module, filename) => {
     module.exports = fs.readFileSync(filename, 'utf8');
 };
 
-// html imports
-const debugLayout = require('./layouts/debug.html');
+// actions
+const doDebug = require('./actions/debug');
 
-// main
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello');
 });
 
-app.get('/debug', (request, response) => {
-    const query = request.query.queryData;
-
-    if (!query) {
-        const html = debugLayout.split('{msg}').join('Query is empty');
-        response.send(html);
-    } else {
-        const pgClient = new PgClient();
-        pgClient
-            .runQueryAsync(query)
-            .then(res => {
-                let msg = '';
-                if (res.rows) {
-                    for (let row of res.rows) {
-                        msg += JSON.stringify(row);
-                    }
-                }
-
-                const html = debugLayout.split('{msg}').join(msg);
-                response.send(html);
-            })
-            .catch(errP => {
-                response.send(errP);
-            });
-    }
-});
+app.get('/debug', doDebug);
 
 app.listen(process.env.PORT);
